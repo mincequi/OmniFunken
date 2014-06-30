c8dfa69b22b42cf401535e241f0877b23e7be83f
mincequi <mincequi@web.de>
2014-06-30 11:09:15 +0200

    Update rtpreceiver.h

--- rtpreceiver.h
+++ rtpreceiver.h
@@ -7,13 +7,22 @@ class RtpReceiver : public QObject
 {
     Q_OBJECT
 public:
+    enum PayloadType {
+        TimingRequest       = 82,
+        TimingResponse      = 83,
+        Sync                = 84,
+        RetransmitRequest   = 85,
+        RetransmitResponse  = 86,
+        AudioData           = 96
+    };
+
     explicit RtpReceiver(QObject *parent = 0);
 
 signals:
-    void newSocket();
+    //void socketRequired();
 
 public slots:
-    //void
+    void setup();
 
 };
 

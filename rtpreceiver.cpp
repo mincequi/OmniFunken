12dcfae6a42dc610b25abb595d8dbbbb8f976fce
mincequi <mincequi@web.de>
2014-06-30 16:37:29 +0200

    Update rtpreceiver.cpp

--- rtpreceiver.cpp
+++ rtpreceiver.cpp
@@ -4,3 +4,9 @@ RtpReceiver::RtpReceiver(QObject *parent) :
     QObject(parent)
 {
 }
+
+void RtpReceiver::bindSocket(PayloadType payloadType, quint16 *port)
+{
+    if (!m_udpSocket) m_udpSocket = new QUdpSocket();
+    *port = 55555;
+}

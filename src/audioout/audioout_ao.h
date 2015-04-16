6cff864582437c54929f63c77d58b13ec9045076
genius <genius@gpt-8.2.0-IDI>
2015-04-16 17:20:40 +0200

    Replace override keyword with Q_DECL_OVERRIDE for GCC 4.6.X

--- src/audioout/audioout_ao.h
+++ src/audioout/audioout_ao.h
@@ -11,12 +11,12 @@ class AudioOutAo : public AudioOutAbstract
 public:
     AudioOutAo();
 
-    virtual const char *name() const override;
-    virtual bool init(const QSettings::SettingsMap &settings) override;
-    virtual void deinit() override;
-    virtual void start() override;
-    virtual void stop() override;
-    virtual void play(char *data, int samples) override;
+    virtual const char *name() const Q_DECL_OVERRIDE;
+    virtual bool init(const QSettings::SettingsMap &settings) Q_DECL_OVERRIDE;
+    virtual void deinit() Q_DECL_OVERRIDE;
+    virtual void start() Q_DECL_OVERRIDE;
+    virtual void stop() Q_DECL_OVERRIDE;
+    virtual void play(char *data, int samples) Q_DECL_OVERRIDE;
 
 private:
     int         m_driverId;
